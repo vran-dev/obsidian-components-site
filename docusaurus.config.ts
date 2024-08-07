4aaef226ef53840c2c9d5b5129ab73fc220909e3
vran <vran_dev@foxmail.com>
2024-08-07 21:38:58 +0800

    feat: update url config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: './img/favicon-128.ico',
 
   // Set the production url of your site here
-  url: 'https://obsidian-components.github.io',
+  url: 'https://cp.cc1234.cc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/obsidian-components-site',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
