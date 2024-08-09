de0a23c516ad4d5b747965b4b4b5019bc4f65622
vran <vran_dev@foxmail.com>
2024-08-09 16:18:00 +0800

    feat: update feature page

--- src/components/Home/Feature/Feature.tsx
+++ src/components/Home/Feature/Feature.tsx
@@ -7,7 +7,7 @@ export default function () {
     {
       title: "个性主页",
       icon: <Home className={`text-pink-500`} />,
-      description: "每个人都可以打造属于自己的独一无二的 Obsidian 主页",
+      description: "打造独一无二的 Obsidian 主页",
       image: "./img/feature/home-page.png",
       link: {
         url: "https://wxycbt0cjk.feishu.cn/wiki/UrTqwG98Lij4aLkczVbcOxX7njg",
@@ -17,7 +17,7 @@ export default function () {
     {
       title: "项目管理",
       icon: <PanelsTopLeft className={`text-orange-500`} />,
-      description: "在一个地方计划和管理项目，数据一览无余",
+      description: "项目规划、管理、复盘，数据一览无余",
       image: "./img/feature/project-management.png",
       link: {
         url: "https://wxycbt0cjk.feishu.cn/wiki/QdHVwJqXmix3phkeVPucPvFGnNd",
@@ -27,7 +27,7 @@ export default function () {
     {
       title: "习惯养成",
       icon: <Target className={`text-blue-500`} />,
-      description: "计划和培养习惯，成为一个长期价值主义的实践者",
+      description: "科学培养习惯，践行长期价值主义",
       image: "./img/feature/habit-tracker.png",
       link: {
         url: "https://wxycbt0cjk.feishu.cn/wiki/R6vjwvnJtiHlyHknf80cQ8X5nBb",
@@ -37,7 +37,7 @@ export default function () {
     {
       title: "书库影城",
       icon: <Book className={`text-amber-500`} />,
-      description: "记录你的阅读和观影历程，与时间成为朋友",
+      description: "记录你的阅读和观影，与时间成为朋友",
       image: "./img/feature/books.png",
       link: {
         url: "",
@@ -45,9 +45,9 @@ export default function () {
       },
     },
     {
-      title: "瀑布流",
+      title: "相册瀑布流",
       icon: <Image className={`text-amber-500`} />,
-      description: "不止是图片，是 Obsidian 缺失的附件管理库",
+      description: "以另一种方式浏览 Obsidian 里的图片",
       image: "./img/feature/images.png",
       link: {
         url: "https://wxycbt0cjk.feishu.cn/wiki/L1Jhw7HeSiJXtYkItc5cfb0Fnif",
@@ -57,7 +57,10 @@ export default function () {
   ];
 
   return (
-    <div id="feature" className="flex flex-col gap-4 px-8 bg-slate-50 dark:bg-transparent py-2 w-full">
+    <div
+      id="feature"
+      className="flex flex-col gap-4 px-8 bg-slate-50 dark:bg-transparent py-2 w-full"
+    >
       <div className="w-full text-center text-4xl p-4 tracking-wider text-stone-600 dark:text-stone-200 font-bold">
         来了解一下。
       </div>
@@ -65,7 +68,7 @@ export default function () {
         想象不再受限 🎉
       </div>
 
-      <div className="flex flex-col w-full gap-4 p-4 md:items-center lg:flex-row lg:items-stretch lg:justify-center">
+      <div className="flex flex-col w-full gap-4 p-4 md:p-0 md:items-center lg:flex-row lg:items-stretch lg:justify-center">
         {data.map((item, index) => {
           return (
             <div
@@ -73,8 +76,8 @@ export default function () {
               className={`cursor-pointer w-full flex flex-col items-center gap-4 rounded-md bg-white dark:bg-transparent dark:text-stone-100  dark:border dark:border-stone-400 dark:hover:border-stone-200 shadow text-stone-800 font-bold p-4 hover:bg-neutral-50`}
             >
               {item.icon}
-              {item.title}
-              <div className="w-full text-center text-stone-400 dark:text-stone-300 tracking-wider">
+              <span className="text-base md:text-2xl">{item.title}</span>
+              <div className="w-full text-center text-stone-500 dark:text-stone-300 tracking-wider text-base md:text-xl">
                 {item.description}
               </div>
               <div>
@@ -86,7 +89,7 @@ export default function () {
                 </a>
               </div>
 
-              <div className="border-gray-200 w-full rounded-md p-4">
+              <div className="border-gray-200 w-full rounded-md p-1">
                 <Zoom>
                   <img src={item.image} className="w-full" />
                 </Zoom>
